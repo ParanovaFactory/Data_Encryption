9b246016eea36529c03c02cf54aaa5e299dc7698
ParanovaFactory <berkay_krdmn@hotmail.com>
2024-10-03 15:40:41 +0300

    Add project files.

---
+++ DataEncryption/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace DataEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAccountNo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDecoing = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(105, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(151, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 172);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(664, 335);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Encrypted Datas";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(658, 310);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(105, 44);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(151, 26);
+            this.txtSurname.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Surname";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(105, 76);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(151, 26);
+            this.txtMail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mail";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(105, 108);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(151, 26);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password";
+            // 
+            // txtAccountNo
+            // 
+            this.txtAccountNo.Location = new System.Drawing.Point(105, 140);
+            this.txtAccountNo.Name = "txtAccountNo";
+            this.txtAccountNo.Size = new System.Drawing.Size(151, 26);
+            this.txtAccountNo.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Accoun No";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(262, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(103, 58);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDecoing
+            // 
+            this.btnDecoing.Location = new System.Drawing.Point(262, 76);
+            this.btnDecoing.Name = "btnDecoing";
+            this.btnDecoing.Size = new System.Drawing.Size(103, 58);
+            this.btnDecoing.TabIndex = 14;
+            this.btnDecoing.Text = "Decoding";
+            this.btnDecoing.UseVisualStyleBackColor = true;
+            this.btnDecoing.Click += new System.EventHandler(this.btnDecoing_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(372, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(284, 154);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 516);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDecoing);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAccountNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAccountNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDecoing;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DataEncryption/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=predator;Initial Catalog=Encryption;Integrated Security=True;TrustServerCertificate=True");
+
+        void listCrypted()
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from TblData", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            byte[] bName = ASCIIEncoding.ASCII.GetBytes(name);
+            string cName = Convert.ToBase64String(bName);
+
+            string surname = txtSurname.Text;
+            byte[] bSurname = ASCIIEncoding.ASCII.GetBytes(surname);
+            string cSurname = Convert.ToBase64String(bSurname);
+
+            string mail = txtMail.Text;
+            byte[] bMail = ASCIIEncoding.ASCII.GetBytes(mail);
+            string cMail = Convert.ToBase64String(bMail);
+
+            string password = txtPassword.Text;
+            byte[] bPassword = ASCIIEncoding.ASCII.GetBytes(password);
+            string cPassword = Convert.ToBase64String(bPassword);
+
+            string accountNo = txtAccountNo.Text;
+            byte[] bAccountNo = ASCIIEncoding.ASCII.GetBytes(accountNo);
+            string cAccountNo = Convert.ToBase64String(bAccountNo);
+
+            conn.Open();
+            SqlCommand command = new SqlCommand("insert into TblData (Name,Surname,Mail,Password,AccountNo) values (@p1,@p2,@p3,@p4,@p5)", conn);
+            command.Parameters.AddWithValue("@p1", cName);
+            command.Parameters.AddWithValue("@p2", cSurname);
+            command.Parameters.AddWithValue("@p3", cMail);
+            command.Parameters.AddWithValue("@p4", cPassword);
+            command.Parameters.AddWithValue("@p5", cAccountNo);
+            command.ExecuteNonQuery();
+            conn.Close();
+            listCrypted();
+        }
+
+        private void btnDecoing_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            byte[] bName = Convert.FromBase64String(name);
+            string cName = ASCIIEncoding.ASCII.GetString(bName);
+
+            string surname = txtSurname.Text;
+            byte[] bSurname = Convert.FromBase64String(surname);
+            string cSurname = ASCIIEncoding.ASCII.GetString(bSurname);
+
+            string mail = txtMail.Text;
+            byte[] bMail = Convert.FromBase64String(mail);
+            string cMail = ASCIIEncoding.ASCII.GetString(bMail);
+
+            string password = txtPassword.Text;
+            byte[] bPassword = Convert.FromBase64String(password);
+            string cPassword = ASCIIEncoding.ASCII.GetString(bPassword);
+
+            string accountNo = txtAccountNo.Text;
+            byte[] bAccountNo = Convert.FromBase64String(accountNo);
+            string cAccountNo = ASCIIEncoding.ASCII.GetString(bAccountNo);
+
+            richTextBox1.Text = cName + " " + cSurname + " " + cMail + " " + cPassword + " " + cAccountNo;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtSurname.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtMail.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtPassword.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txtAccountNo.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listCrypted();
+        }
+    }
+}
